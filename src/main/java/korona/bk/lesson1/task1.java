49b9ea61d47f3a7f54d195ec97c4efd6df7e2afc
Art Skl <artyom.skl@gmail.com>
2025-03-18 22:00:27 +0300

    Task1 & Task2

--- src/main/java/korona/bk/lesson1/task1.java
+++ src/main/java/korona/bk/lesson1/task1.java
@@ -3,21 +3,18 @@ import java.util.Arrays;
 
 public class task1 {
     public static void main(String[] args) {
-        sortIntegerArray();
-        sortCharArray();
-    }
 
-    public static void sortIntegerArray() {
         int[] array = {8, 6, 9, 7, 4, 9, 0};
         System.out.println("Tel No b4 sort: " + Arrays.toString(array));
         Arrays.sort(array);
         System.out.println("Tel No after sort: " + Arrays.toString(array));
-    }
 
-    public static void sortCharArray() {
+
+
         char[] charArray = {'A', 'r', 't', 's', 'e', 'm', 'O', 'h', 'u', 'e', 'n', 'e', 'n'};
         System.out.println("Artsem Ohuenen b4 sort: " + Arrays.toString(charArray));
         Arrays.sort(charArray);
         System.out.println("Artsem Ohuenen after sort: " + Arrays.toString(charArray));
-    }
+
+}
 }
